4f6da71a29fae3dd0bcea221138945886145dbff
Shawan <pnt.ats@gmail.com>
2021-07-03 02:40:44 +0600

    vagrant and kubernetes env deploy

---
+++ test2/dependency-installation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo bash
+apt update
+uname="$1" || username="$(whoami)"
+
+# installing libvirt
+apt install qemu-system libvirt-clients libvirt-daemon-system
+apt install virt-manager
+adduser ${username} libvirt
+cat >> ~/.bashrc<<EOF
+export LIBVIRT_DEFAULT_URI='qemu:///system'
+EOF
+source ~/.bashrc
+
+#installing vagrant
+apt install vagrant-libvirt libvirt-daemon-system
+
+#installing ansible
+apt install ansible
